fad9089b0c3fa5d43beb6cea05ba320e8e841e57
qospanulan <kospanovulan@gmail.com>
2025-04-23 21:23:43 +0500

    admin panel 1

--- django2/authorization/admin.py
+++ django2/authorization/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.hashers import make_password
 
 
 @admin.register(get_user_model())
 class CustomUserAdmin(ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_active')
+    list_editable = ('role', 'is_active')
+    list_filter = ('role', 'is_active')
+    search_fields = ("username", "first_name", "last_name", "email")
+    # ordering = ("username", "email")
+    list_display_links = ('username', 'email')
+
+    actions = ()
 
     def save_model(self, request, obj, form, change):
         if not obj.password.startswith("pbkdf2_sha256"):
